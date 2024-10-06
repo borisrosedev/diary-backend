00ddb0b56e5dafc0dfa8a0e6179a4973d17a9ba5
borisrose <boris.alexandre.rose@gmail.com>
2024-10-06 21:18:34 +0200

    clean/v3/multer

--- app/routes/file.route.js
+++ app/routes/file.route.js
@@ -1,12 +1,6 @@
 import express from "express"
-import authMiddleware from "../middlewares/auth.middleware.js"
 import fileController from "../controllers/file.controller.js"
-import fileValidatorsMiddleware from "../middlewares/file-validators.middleware.js"
-import multer from "multer"
-import path from "path"
 import { imageUpload, videoUpload } from "../config/multer.js"
-
-
 
 const route = express.Router()
 
